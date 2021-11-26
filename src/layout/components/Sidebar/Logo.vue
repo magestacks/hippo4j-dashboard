5160da0fc24c0a5ecafba733bcca3a7a82f7ed84
chen.ma <machencoding@163.com>
2021-11-26 23:08:56 +0800

    auto commit

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -24,8 +24,9 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Element Admin',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: 'Hippo4J 1.0.0',
+      logo: 'https://images-machen.oss-cn-beijing.aliyuncs.com/hippo4j-logo.png'
+      // logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
 }
