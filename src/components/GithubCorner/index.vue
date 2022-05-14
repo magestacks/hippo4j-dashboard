725568d41b62d24f4bfbb51cffbfd10be2e2f882
chen.ma <machencoding@163.com>
2022-05-14 23:02:24 +0800

    auto commit

--- src/components/GithubCorner/index.vue
+++ src/components/GithubCorner/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/acmenlt/dynamic-threadpool" target="_blank" class="github-corner" aria-label="View source on Github">
+  <a href="https://github.com/longtai-cn/hippo4j" target="_blank" class="github-corner" aria-label="View source on Github">
     <svg
       width="80"
       height="80"
